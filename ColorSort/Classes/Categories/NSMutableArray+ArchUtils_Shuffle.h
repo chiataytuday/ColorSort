6da3370d254c4599e47044d1e9ddae976532b057
Frankie Laguna <frankielaguna@gmail.com>
2012-04-05 23:10:29 -0400

    Updated copyright information
    - Added the source URL for the NSMutableArray shuffle category
    - Changed org name

--- ColorSort/Classes/Categories/NSMutableArray+ArchUtils_Shuffle.h
+++ ColorSort/Classes/Categories/NSMutableArray+ArchUtils_Shuffle.h
@@ -1,9 +1,10 @@
+// From: http://stackoverflow.com/questions/791232/canonical-way-to-randomize-an-nsarray-in-objective-c
 //
 //  NSMutableArray+ArchUtils_Shuffle.h
 //  ColorTest
 //
 //  Created by Frankie Laguna on 4/4/12.
-//  Copyright (c) 2012 The Atom Group. All rights reserved.
+//  Copyright (c) 2012 Frankie Laguna. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- ColorSort/Classes/Custom Views/VerticalProgressBar.m
+++ ColorSort/Classes/Custom Views/VerticalProgressBar.m
@@ -3,7 +3,7 @@
 //  ColorTest
 //
 //  Created by Frankie Laguna on 4/5/12.
-//  Copyright (c) 2012 The Atom Group. All rights reserved.
+//  Copyright (c) 2012 Frankie Laguna. All rights reserved.
 //
 
 #import <QuartzCore/QuartzCore.h>
